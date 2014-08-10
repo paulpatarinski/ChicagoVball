84f216fd68488708f1e9b07bb9577e38bd009936
ppatarinski <paulpatarinski@gmail.com>
2014-08-10 12:18:27 -0500

    Initial commit

---
+++ ChiVball/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ChiVball
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new MainPage ();
+		}
+	}
+}
+
---
+++ ChiVball/Pages/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ChiVball
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			Content = new Label {
+				Text = "Hello, Forms!",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+		}
+	}
+}
